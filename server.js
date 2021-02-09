f906805ae0f1d60dc689f6e403ce80847a832b0c
Cesar Preza <cpreza93@yahoo.com>
2021-02-08 21:17:10 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname + 'client/build')));
+
+app.listen(port, () => console.log('listening on port 3000'));
