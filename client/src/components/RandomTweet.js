058edef70d2f3197e8a451daf07682f98f54a036
Cesar Preza <cpreza93@yahoo.com>
2021-02-28 21:48:01 -0800

    user pictures added

--- client/src/components/RandomTweet.js
+++ client/src/components/RandomTweet.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import nasaImage from '../images/nasa.png';
+import espnImage from '../images/espn.png';
+import laKingsImage from '../images/la_kings.png';
+import disneyImage from '../images/disney.png';
+import starWarsImage from '../images/star-wars.jpg';
 
 class RandomTweet extends Component {
     constructor(props) {
@@ -31,15 +36,25 @@ class RandomTweet extends Component {
                         <div>
                             <h2>Select a user to display a random tweets from that acount</h2>
                         </div>
-                        <div className='tweetCard' > 
-                            {this.state.tweet !== null ?
-                                <div>
-                                    <p>username: {this.state.tweet.user.screen_name} </p>
-                                    <p>tweet: { this.state.tweet.text }</p>
-                                </div> :
-                                null }
-                            
+                        <div className='tweet-container' >
+                            <div className='tweet-card'> 
+                                <figure>
+                                    <img classaName='nasa-image' src={laKingsImage}></img>
+                                    <img classaName='nasa-image' src={espnImage}></img>
+                                    <img classaName='nasa-image' src={nasaImage}></img>
+                                    <img classaName='nasa-image' src={starWarsImage}></img>
+                                    <img classaName='nasa-image' src={disneyImage}></img>
+                                </figure>
+                                    {this.state.tweet !== null ?
+                                        <div>
+                                            <p>username: {this.state.tweet.user.screen_name} </p>
+                                            <p>tweet: { this.state.tweet.text }</p>
+                                        </div> :
+                                        null }
+                                    
+                                </div>
                         </div>
+                            
                     </div>
                 </main>
             </div>
