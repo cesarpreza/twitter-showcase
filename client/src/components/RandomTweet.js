fd81795a89f217104905cf511d31daeac4663dac
Cesar Preza <cpreza93@yahoo.com>
2021-03-02 20:48:40 -0800

    random tweet from nasa

--- client/src/components/RandomTweet.js
+++ client/src/components/RandomTweet.js
@@ -22,9 +22,10 @@ class RandomTweet extends Component {
 
     async getTweets(url) {
         const tweetUrl = await axios.get(url);
-        const tweetResponse = tweetUrl.data.statuses[0];
-        this.setState({ tweet: tweetResponse });
-        console.log(tweetResponse)
+        const tweetResponse = tweetUrl.data.statuses;
+        const randomTweet = tweetResponse[Math.floor(Math.random() * tweetResponse.length)];
+        this.setState({ tweet: randomTweet });
+        console.log(randomTweet);
     }
 
     render() {
@@ -39,12 +40,19 @@ class RandomTweet extends Component {
                         <div className='tweet-container' >
                             <div className='tweet-card'> 
                                 <figure>
-                                    <img classaName='nasa-image' src={laKingsImage}></img>
-                                    <img classaName='nasa-image' src={espnImage}></img>
-                                    <img classaName='nasa-image' src={nasaImage}></img>
-                                    <img classaName='nasa-image' src={starWarsImage}></img>
-                                    <img classaName='nasa-image' src={disneyImage}></img>
+                                    <img className='image' src={laKingsImage}></img>
+                                    <img className='image' src={espnImage}></img>
+                                    <img className='image' src={nasaImage}></img>
+                                    <img className='image' src={starWarsImage}></img>
+                                    <img className='image' src={disneyImage}></img>
                                 </figure>
+                                <div>
+                                    <button id='kingsButton'>Click</button>
+                                    <button id='espnButton'>Click</button>
+                                    <button id='nasaButton'>Click</button>
+                                    <button id='starWarsButton'>Click</button>
+                                    <button id='disneyButton'>Click</button>
+                            </div>
                                     {this.state.tweet !== null ?
                                         <div>
                                             <p>username: {this.state.tweet.user.screen_name} </p>
