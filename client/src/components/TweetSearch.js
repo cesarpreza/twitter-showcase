fe5f004b2f53bc83c6d4ad0a51caf430eb33b58e
Cesar Preza <cpreza93@yahoo.com>
2021-03-08 21:30:10 -0800

    setting state to search bar

--- client/src/components/TweetSearch.js
+++ client/src/components/TweetSearch.js
@@ -1,12 +1,36 @@
 import React,  { Component } from 'react';
 
 class TweetSearch extends Component {
+    constructor() {
+        super()
+        this.state = {
+            searchTerm: '',
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.HandleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ searchTerm: e.target.value })
+        
+        
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log('submit');
+    }
+
     render() {
+        console.log(this.state.searchTerm);
         return (
             <div className='page-container'>
                 <div>
-                    <h1>Search Page</h1>
-                    <input type='text' placeholder='search for a tweet..'></input>
+                    <h1>Search for a twitter user</h1>
+                    <form onChange={this.handleChange}>
+                        <input type='text' placeholder='search for a tweet..'></input>
+                        <button onClick={this.handleSubmit}>Search</button>
+                    </form>
                 </div>
             </div>
         )
