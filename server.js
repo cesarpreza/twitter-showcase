cf19b11546b981fc0462d8a82d9aeca48fd1f4b4
Cesar Preza <cpreza93@yahoo.com>
2021-03-19 16:25:07 -0700

    deployment fix

--- server.js
+++ server.js
@@ -1,35 +1,18 @@
 const axios = require('axios');
 const express = require('express');
 const path = require('path');
-const port = 3000;
-const cors = require('cors');
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 const token = process.env.TOKEN;
 
 
 const app = express();
 
-const whitelist = ['http://localhost:3000']
-const corsOptions = {
-    origin: function (origin, callback) {
-        console.log("** Origin of request " + origin)
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            console.log("Origin acceptable")
-        callback(null, true)
-    } else {
-        console.log("Origin rejected")
-        callback(new Error('Not allowed by CORS'))
-    }
-    }
-}
-app.use(cors(corsOptions))
+app.use('/', express.static(path.join(__dirname, 'client/build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+})
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client/build')));
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    })
-}
 
 app.listen(port, () => console.log('listening on port 3000'));
 
